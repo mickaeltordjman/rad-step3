868f2a635c49919fdc564f6b445a92f2910cd270
mickaeltordjman <mickael_tordjman@hotmail.com>
2025-08-12 05:57:54 +0200

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 const FILES_ENDPOINT  = "./assets/images.json"; 
-const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycbzFAJHSkxlcd7yXh-fl1vDOZIgD65Ts-QUyEzJVNfpnpuVpFqK7Wkwv2uezN5DuF3Wg/exec";
+const SUBMIT_ENDPOINT = "https://script.google.com/macros/s/AKfycby8IPI0GR3wB9PSnnXK2NZkQ7IHEaIJRaGrlXyBiwT_1bqC0kUAD8-Wrc8FuLZE1zvp/exec";
 
 const LIMIT_IMAGES = null;
 const KEY_REAL = "r";
